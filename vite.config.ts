9c2db1e1f5f36acf5bbb318e9ae484ae8845f06b
Mikhail Polskov <misapolskov9@gmail.com>
2025-08-06 23:34:09 +0300

    fix: update vite config for production server

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    host: true,
+    port: 8080,
+    host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: 'http://185.185.69.254:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false
       }
